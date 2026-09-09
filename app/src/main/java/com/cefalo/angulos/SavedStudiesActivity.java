d710b235987b80d435d725dbd75224d0f0b44b5c
yomismtz <yomismtz@gmail.com>
2026-09-09 17:26:01 -0600

    Use AppCompat dialogs for DayNight support

--- app/src/main/java/com/cefalo/angulos/SavedStudiesActivity.java
+++ app/src/main/java/com/cefalo/angulos/SavedStudiesActivity.java
@@ -1,6 +1,6 @@
 package com.cefalo.angulos;
 
-import android.app.AlertDialog;
+import androidx.appcompat.app.AlertDialog;
 import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;

780cd1ed129f93e6c1278b817258764f1970e061
yomismtz <yomismtz@gmail.com>
2026-09-09 08:17:35 -0600

    Add Android app project files

---
+++ app/src/main/java/com/cefalo/angulos/MeasurementDefinition.java
@@ -0,0 +1,53 @@
+package com.cefalo.angulos;
+
+public class MeasurementDefinition {
+    public enum Type { THREE_POINTS, TWO_LINES }
+
+    public final String name;
+    public final Type type;
+    public final String[] pointLabels;
+    public final double normalMin;
+    public final double normalMax;
+    public final String normText;
+    public final String instruction;
+    public final String lowDiagnosis;
+    public final String normalDiagnosis;
+    public final String highDiagnosis;
+    public final boolean chooseSupplementClosestToNorm;
+
+    public MeasurementDefinition(
+            String name,
+            Type type,
+            String[] pointLabels,
+            double normalMin,
+            double normalMax,
+            String normText,
+            String instruction,
+            String lowDiagnosis,
+            String normalDiagnosis,
+            String highDiagnosis,
+            boolean chooseSupplementClosestToNorm) {
+        this.name = name;
+        this.type = type;
+        this.pointLabels = pointLabels;
+        this.normalMin = normalMin;
+        this.normalMax = normalMax;
+        this.normText = normText;
+        this.instruction = instruction;
+        this.lowDiagnosis = lowDiagnosis;
+        this.normalDiagnosis = normalDiagnosis;
+        this.highDiagnosis = highDiagnosis;
+        this.chooseSupplementClosestToNorm = chooseSupplementClosestToNorm;
+    }
+
+    public String diagnosis(double value) {
+        if (value < normalMin) return lowDiagnosis;
+        if (value > normalMax) return highDiagnosis;
+        return normalDiagnosis;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
